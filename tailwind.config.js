35a8c6fe3a540c875d572f4191e2972f09b7749b
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-12-20 10:43:35 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  webpackFinal: async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            plugins: [require("tailwindcss"), require("autoprefixer")],
+          },
+        },
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+    return config;
+  },
+};
